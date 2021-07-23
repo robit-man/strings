777a5003a191e419e305a3979179e9c170b00ef6
nu-soccr <8105@protonmail.com>
2021-07-23 05:40:42 +0000

    whateverthehe;ll

---
+++ src/pages/jeff.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Router, Link, Route} from "svelte-routing";
+    import {slide} from 'svelte/transition';
+
+</script>
+
+<main>
+<div class="greg"></div>
+
+</main>
+
+<style>
+
+
+</style>
