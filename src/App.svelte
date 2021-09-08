cc93dc5f629172f35bd9e6cd9c3063002000bfdc
pointblank <pointblank.connect@gmail.com>
2021-09-08 19:24:20 +0000

    home

--- src/App.svelte
+++ src/App.svelte
@@ -29,19 +29,24 @@
         </div>
       </header>
     <Router url="{url}">
-        
+        {#if url === '/mint'}
+        <Link style="top:0px;left:opx;position:fixed;z-index:6;" to="/">
+            <img class="mint" style="margin:2.5rem;" height="48px" src="/imgs/home.png" alt=""></Link>
+        {:else}
         <Link style="top:0px;left:opx;position:fixed;z-index:6;" to="/mint">
             <img class="mint" style="margin:2.5rem;" height="48px" src="/imgs/mint.png" alt=""></Link>
-
+        
+        {/if}
         <Route path="/"><Home /></Route>
         <Route path="/mint" component={Mint}><Mint /></Route>
+        <Route path="/mint"><Mint /></Route>
         <Route path="/about" component={About}><About /></Route>
         <Route path="/*"><Four /></Route>
 
     </Router>
 
     <script type="text/javascript" src="/script.js"></script>
-    <footer style="position:absolute;bottom:0px;left:0px;width:100vw;height:auto;">
+    <footer style="">
         <a href="/"><h1 class="pepebottom">SPACEPEPES</h1></a>
     </footer>
 </div>
@@ -51,7 +56,6 @@
 		padding: 0rem;
 		margin: 0 auto;
 	}
-    header{z-index:5;position:absolute;}
     .nav-row{
   position: fixed;
   height:100px;top:0px;width:calc(100vw - 4rem);
@@ -68,10 +72,12 @@
 .pepebottom{
     width:100vw;text-align:center;font-size:5rem;
 }
-@media screen and (max-width: 1000px) {
-        .pepebottom{width:100vw;text-align:center;font-size:3rem;}
-  
-    }
+header{z-index:5;position:relative;}
+
+.pepebottom{width:100vw;text-align:center;font-size:2rem;}
+footer{position:relative;bottom:0px;left:0px;width:100vw;height:auto;}
+
+
 .nav-links{display:flex;flex-flow:row;justify-content:space-around;}
 .nav-links > a{margin:1rem;transition:transform 0.2s ease;}
 .icon-box > a{margin:1rem;transition:transform 0.2s ease;}
