b034d98b368436cced8f4f14c39550470924134b
pointblank <pointblank.connect@gmail.com>
2021-10-15 18:40:28 +0000

    update

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -119,8 +119,20 @@ var metadata = {
 
 </section>
 <section class="third">
-  <img src="/imgs/arrowbottom.svg" alt="" class="arrowtop">
-  <div class="constraint"> 
+  <div style="width: 100vw;position:relative;top:-10px;left:0px;transform:rotate(180deg);">
+    <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+    <defs>
+    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+    </defs>
+    <g class="parallax">
+    <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+    <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+    <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+    <use xlink:href="#gentle-wave" x="48" y="7" fill="#f0f0f0" />
+    </g>
+    </svg>
+    </div>  <div class="constraint"> 
     <p style="font-size:3rem;font-weight:bold;">Attributes and Rarities Showcase</p>
 <div class="attribute-list-wrapper">
     
